9a03d0e23965f4f980fa22e64db0be51f5098c39
shuso1213 <shuso1213@gmail.com>
2020-05-08 20:50:53 +0900

    Initial Commit

---
+++ gitTest2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  gitTest2
+//
+//  Created by しゅん on 2020/05/08.
+//  Copyright © 2020 shun050501. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
